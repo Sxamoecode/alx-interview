bb9c2ebfb1cced20aba266cc5e1366e41228f5d8
Sxamoecode <samuelayandare@gmail.com>
2023-08-01 11:24:25 -0800

    Done

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -7,7 +7,7 @@ import sys
 solutions = []
 """The list of possible solutions to the N queens problem.
 """
-n = 0
+g = 0
 """The size of the chessboard.
 """
 pos_soln = None
@@ -20,27 +20,27 @@ def get_input():
     Returns:
         int: The size of the chessboard.
     """
-    global n
-    n = 0
+    global g
+    g = 0
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
     try:
-        n = int(sys.argv[1])
+        g = int(sys.argv[1])
     except Exception:
         print("N must be a number")
         sys.exit(1)
-    if n < 4:
+    if g < 4:
         print("N must be at least 4")
         sys.exit(1)
-    return n
+    return g
 
 
 def is_attacking(pos0, pos1):
     """the positions of two queens are in an attacking mode.
     Args:
         pos0 (list or tuple): 1st queen's position.
-        pos1 (list or tuple): 2ndqueen's position.
+        pos1 (list or tuple): 2nd queen's position.
     Returns:
         bool: True if the queens are in an attacking position else False.
     """
@@ -63,7 +63,7 @@ def group_exists(group):
             for grp_pos in group:
                 if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
                     i += 1
-        if i == n:
+        if i == g:
             return True
     return False
 
@@ -75,14 +75,14 @@ def build_solution(row, group):
         group (list of lists of integers): The group of valid positions.
     """
     global solutions
-    global n
-    if row == n:
+    global g
+    if row == g:
         tmp0 = group.copy()
         if not group_exists(tmp0):
             solutions.append(tmp0)
     else:
-        for col in range(n):
-            a = (row * n) + col
+        for col in range(g):
+            a = (row * g) + col
             matches = zip(list([pos_soln[a]]) * len(group), group)
             used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
             group.append(pos_soln[a].copy())
@@ -94,14 +94,14 @@ def build_solution(row, group):
 def get_solutions():
     """Gets the solutions for the given chessboard size.
     """
-    global pos_soln, n
-    pos_soln = list(map(lambda x: [x // n, x % n], range(n ** 2)))
+    global pos_soln, g
+    pos_soln = list(map(lambda x: [x // n, x % n], range(g ** 2)))
     a = 0
     group = []
     build_solution(a, group)
 
 
-n = get_input()
+g = get_input()
 get_solutions()
 for solution in solutions:
     print(solution)
